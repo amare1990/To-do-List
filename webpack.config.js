7062335c8bd79ca5d06169cabd56c7f906c6e18f
amare1990 <amaremek@gmail.com>
2022-10-05 00:14:42 +0300

    Upgrade node.js and npm to the latest stable version

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry:  {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
 
     new HtmlWebpackPlugin({
@@ -13,9 +18,14 @@ module.exports = {
 
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+
+    runtimeChunk: 'single',
+
   },
   module: {
 
