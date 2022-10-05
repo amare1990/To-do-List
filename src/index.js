f80522bc5462397830bd0c9f9c22239a8e1ea834
amare1990 <amaremek@gmail.com>
2022-10-05 16:26:41 +0300

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -5,29 +5,32 @@ const toDoTasks = [
   {
     Index: 0,
     description: "Finish the Profesional Skils Project",
-  
+    completed: false
   },
   {
     Index: 1,
     description: "Read All the lessons",
-   
+    completed: false
   },
   {
     Index: 2,
     description: "Finish today's project",
-    
+    completed: false    
   },
 ];
 
 const wrapper = document.querySelector('.wrapper');
 wrapper.innerHTML = `
-<div class ="task-lists title"> 
-<p> Today's To Do </p>
-<i class="fa fa-refresh"></i>  
+<div class ="task-lists title part"> 
+<p class ="opacity"> Today's To Do </p>
+<button type = "button" class = "btn refresh">
+  <i class="fa fa-refresh"></i>  
 </div>
-<div class = "add-part">
-  <input type = "text" id = "input-task" name = "task-add" placehlder = "Add our list here ...>
-  <i class='fas fa-level-up-alt'></i>
+<div class = "add-part part">
+  <input type = "text" id = "input-task" name = "task-add" placeholder = "Add your list here ..." class ="opacity">
+ <button type = "button" class = "btn">
+ <i class="fa-solid fa-turn-down-left"></i>
+ </button>
 </div>`;
 const displayTasks = () => {
   for(let i = 0; i < toDoTasks.length; i++) {
@@ -35,16 +38,22 @@ const displayTasks = () => {
     task.className = 'task';
     task.innerHTML = `
     
-    <div class ="task-lists">
+    <div class ="task-lists part">
       <div>
-        <input type="checkbox" id="${toDoTasks[i].Index}" value="${toDoTasks[i].description}" name = "Task" class = "input-task">
-        <label for="${toDoTasks[i].Index}"> ${toDoTasks[i].description}  </label>
+        <input type="checkbox" id="${toDoTasks[i].Index}" value="${toDoTasks[i].description}" name = "Task" class = "input-task opacity">
+        <label class = "opacity" for="${toDoTasks[i].Index}"> ${toDoTasks[i].description}  </label>
       </div>
-      <i class="fab fa‑accessible‑icon"></i>
+      <button type = "button" class = "edit-task btn">
+        <i class='fas fa-ellipsis-v'></i>
+      </button>
     </div>
     `;
     wrapper.appendChild(task);
   }
+  const clearBtn = document.createElement('button');
+  clearBtn.className = "clear-btn";
+  clearBtn.innerHTML = "Clear all completed";
+  wrapper.append(clearBtn);
 };
 
 displayTasks();
