145f645d9e01762c97c09851f4e391d19059bde1
amare1990 <amaremek@gmail.com>
2022-10-05 14:27:21 +0300

    Modify the index.js to add font awesome icons

--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
 import _ from 'lodash';
 import './style.css';
 
+const toDoTasks = [
+  {
+    Index: 0,
+    description: "Finish the Profesional Skils Project",
+  
+  },
+  {
+    Index: 1,
+    description: "Read All the lessons",
+   
+  },
+  {
+    Index: 2,
+    description: "Finish today's project",
+    
+  },
+];
 
- function component() {
-   const element = document.createElement('div');
+const wrapper = document.querySelector('.wrapper');
+wrapper.innerHTML = `
+<div class ="task-lists title"> 
+<p> Today's To Do </p>
+<i class="fa fa-refresh"></i>  
+</div>
+<div class = "add-part">
+  <input type = "text" id = "input-task" name = "task-add" placehlder = "Add our list here ...>
+  <i class='fas fa-level-up-alt'></i>
+</div>`;
+const displayTasks = () => {
+  for(let i = 0; i < toDoTasks.length; i++) {
+    const task = document.createElement('div');
+    task.className = 'task';
+    task.innerHTML = `
+    
+    <div class ="task-lists">
+      <div>
+        <input type="checkbox" id="${toDoTasks[i].Index}" value="${toDoTasks[i].description}" name = "Task" class = "input-task">
+        <label for="${toDoTasks[i].Index}"> ${toDoTasks[i].description}  </label>
+      </div>
+      <i class="fab fa‑accessible‑icon"></i>
+    </div>
+    `;
+    wrapper.appendChild(task);
+  }
+};
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+displayTasks();
 
-   return element;
- }
-
- document.body.appendChild(component());
+const inputTask = document.querySelector('.input-task');
+inputTask.style.color = "white";
